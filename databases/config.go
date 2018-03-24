1585d6f70f00b9d909fe17e05e213c3295b68ae9
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-03-24 10:14:09 +0100

    Config

--- databases/config.go
+++ databases/config.go
@@ -1,10 +1,12 @@
 package databases
 
 import (
+	"github.com/cryptounicorns/gluttony/databases/database/clickhouse"
 	"github.com/cryptounicorns/gluttony/databases/database/influxdb"
 )
 
 type Config struct {
-	Type     string `validate:"required"`
-	Influxdb *influxdb.Config
+	Type       string `validate:"required"`
+	Influxdb   *influxdb.Config
+	Clickhouse *clickhouse.Config
 }
