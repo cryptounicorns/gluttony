b7a1a7cba5df8709cf5aea6abb1f0984e7cc1ff6
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-03-24 10:13:18 +0100

    DataRecord

--- databases/record/record.go
+++ databases/record/record.go
@@ -1,3 +1,12 @@
 package record
 
 type Record = interface{}
+
+type DataRecord struct {
+	Timestamp uint64
+	Open      uint64
+	High      uint64
+	Low       uint64
+	Close     uint64
+	Volume    uint64
+}
