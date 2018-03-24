ee339463fab7ff46d68ceb9dc2a1b0d4d210c7cb
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-03-24 09:49:12 +0100

    update deps

--- vendor/github.com/corpix/effects/README.md
+++
@@ -1,6 +0,0 @@
-effects
----------
-
-[![Build Status](https://travis-ci.org/corpix/effects.svg?branch=master)](https://travis-ci.org/corpix/effects)
-
-Side-effects helpers for Go.
--- vendor/github.com/corpix/lua/examples/binding/script.lua
+++
@@ -1,4 +0,0 @@
-local m = require("module")
-print("FooBar in Go called from Lua:", m.foo_bar(1, 2, { a = 2}))
-print("spew.Sdump in Go called from Lua:", m.sdump(m.foo_bar(1, 2, { a = 2})))
-m.printf("fmt.Printf in Go called from Lua: %s -> %s -> %#v\n", m.foo_bar(1, 2, { a = 2}))
